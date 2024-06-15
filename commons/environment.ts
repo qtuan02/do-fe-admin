4698d9e504d74def07140c53d5acba6131f8bfdf
qtuan02 <quoctuan200702@gmail.com>
2024-06-15 17:21:04 +0700

    host

--- commons/environment.ts
+++ commons/environment.ts
@@ -1,7 +1,7 @@
 
 export default class Constants{
-    static readonly URL_V1: string = process.env.API_URL_LOCAL+"/v1";
-    static readonly URL_V2: string = process.env.API_URL_LOCAL+"/v2";
+    static readonly URL_V1: string = process.env.API_URL_HOST+"/v1";
+    static readonly URL_V2: string = process.env.API_URL_HOST+"/v2";
     static readonly PUSHER_KEY: string = process.env.PUSHER_KEY || "";
     static readonly PUSHER_CLUSTER: string = process.env.PUSHER_CLUSTER || "";
 }
